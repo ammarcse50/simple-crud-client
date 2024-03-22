3aef75136a9a7d04f7fe4372f445355b72879701
ammmar <ammaraslam7164@gmail.com>
2024-03-22 14:15:43 +0600

    updated

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+
+    const name = form.name.value;
+
+    const email = form.email.value;
+
+    const user = { name, email };
+
+    console.log(user);
+
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+       
+      });
+  };
+  return (
+    <div className="flex justify-center">
+      <h2>Simple Crud Operation </h2>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" id="" />
+        <br />
+
+        <input type="email" name="email" id="" />
+        <br />
+
+        <input type="submit" value="Add User" />
+      </form>
+    </div>
+  );
+};
+
+export default App;
